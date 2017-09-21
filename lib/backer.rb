e520f79f5e2a5412dccc9f9ac21d40faec2b7d6b
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:19:55 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,14 @@
 class Backer
   attr_accessor :name
 
+  @@backed_projects = []
+
   def initialize(name)
     @name = name
+  end
+
+  self.back_project(title)
+    @@backed_projects << title
   end
 
 end
